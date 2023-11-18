02b7f7d5fe7d60c5a7f6f426ff4443ff45c64b66
sirius1y <sirius1y@outlook.com>
2023-11-18 11:13:05 +0800

    feat: algorithms/算法题模板.md,sync.sh,algorithms/Templates.md

--- posts/Notes/sync.sh
+++ posts/Notes/sync.sh
@@ -1,17 +1,19 @@
+# 定义FILES变量
+FILES=$(cd /home/yoho/projects/blogs/hugo/content/posts/Notes && git status --porcelain | awk '{print $2}' | paste -sd, -)
+
 # 同步笔记到Notes仓库
 cd /home/yoho/projects/blogs/hugo/content/posts/Notes
 git add .
-git commit -m "update: $(git status --porcelain | awk '{print $2}' | paste -sd, -)" # 自动列出更改的文件
-FILES = $(git status --porcelain | awk '{print $2}' | paste -sd, -)
+git commit -m "update: $FILES" # 自动列出更改的文件
 git push
 
 # 发布到博客
 cd ~/projects/blogs/hugo
 hugo
 
-# 切换到博客仓库根目录（如果hugo/public不是仓库根目录的话）
-cd ~/projects/blogs/hugo/public # 添加您的博客仓库的路径
+# 切换到博客仓库根目录
+cd ~/projects/blogs/hugo/public
 git add .
-git commit -m "feat: $FILES" # 自动列出更改的博文
+git commit -m "feat: $FILES" 
 git push origin master
 
