62196a194c07cd069199f4bf5795a1fbb4b45edc
sirius1y <sirius1y@outlook.com>
2023-11-18 11:34:10 +0800

    fix:网站图标不加载问题，把http更改为https

--- public/posts/Notes/sync.sh
+++ public/posts/Notes/sync.sh
@@ -1,4 +1,19 @@
-git pull origin master
+# 定义FILES变量
+FILES=$(cd /home/yoho/projects/blogs/hugo/content/posts/Notes && git status --porcelain | awk '{print $2}' | paste -sd, -)
+
+# 同步笔记到Notes仓库
+cd /home/yoho/projects/blogs/hugo/content/posts/Notes
 git add .
-git commit -m "update"
+git commit -m "update: $FILES" # 自动列出更改的文件
+git push
+
+# 发布到博客
+cd ~/projects/blogs/hugo
+hugo
+
+# 切换到博客仓库根目录
+cd ~/projects/blogs/hugo/public
+git add .
+git commit -m "feat: $FILES" 
 git push origin master
+
