d3e0ac718e91f5d9348ab848e1f100bdff832281
sirius1y <sirius1y@outlook.com>
2023-11-18 11:09:58 +0800

    feat:

--- posts/Notes/sync.sh
+++ posts/Notes/sync.sh
@@ -2,6 +2,7 @@
 cd /home/yoho/projects/blogs/hugo/content/posts/Notes
 git add .
 git commit -m "update: $(git status --porcelain | awk '{print $2}' | paste -sd, -)" # 自动列出更改的文件
+FILES = $(git status --porcelain | awk '{print $2}' | paste -sd, -)
 git push
 
 # 发布到博客
@@ -11,6 +12,6 @@ hugo
 # 切换到博客仓库根目录（如果hugo/public不是仓库根目录的话）
 cd ~/projects/blogs/hugo/public # 添加您的博客仓库的路径
 git add .
-git commit -m "feat: $(git status --porcelain | awk '{print $2}' | paste -sd, -)" # 自动列出更改的博文
+git commit -m "feat: $FILES" # 自动列出更改的博文
 git push origin master
 
