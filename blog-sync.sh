11ae9bea601cdfc46d1d7e5daf2c9f90808761b1
sirius1y <sirius1y@outlook.com>
2023-12-16 01:17:52 +0800

    update:

---
+++ blog-sync.sh
@@ -0,0 +1,29 @@
+# 定义FILES变量
+FILES=$(cd /home/yoho/projects/blogs/hugo/content/posts/Notes && git status --porcelain | awk '{print $2}' | paste -sd, -)
+
+# 同步笔记到sirius2alpha/Notes
+cd /home/yoho/projects/blogs/hugo/content/posts/Notes
+git pull
+git add .
+git commit -m "update: $FILES" # 自动列出更改的文件
+git push
+
+# build
+cd ~/projects/blogs/hugo
+hugo
+
+# 切换到博客仓库根目录
+cd ~/projects/blogs/hugo/public
+git pull
+git add .
+git commit -m "feat: $FILES"
+git push origin master
+
+# 部署到 Nginx 服务器
+NGINX_SERVER_PATH="/var/www/mysites"
+
+# 使用 rsync 同步 public 目录到 Nginx 服务器的相应目录
+rsync -av --delete -e "ssh -p 22" ~/projects/blogs/hugo/public/ root@1.94.126.139:$NGINX_SERVER_PATH
+
+# 重启 Nginx 服务
+ssh root@1.94.126.139 "sudo systemctl reload nginx"
