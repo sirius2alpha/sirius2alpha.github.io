265fd3de6ee6394fb596ef364b7ac4525428520c
sirius1y <sirius1y@outlook.com>
2023-12-17 22:35:40 +0800

    update:

--- blog-sync.sh
+++ blog-sync.sh
@@ -2,22 +2,22 @@
 FILES=$(cd /home/yoho/projects/blogs/hugo/content/posts/Notes && git status --porcelain | awk '{print $2}' | paste -sd, -)
 
 # 同步笔记到sirius2alpha/Notes
-echo start---同步笔记到sirius2alpha/Notes------------------------
+echo ------------------------start---同步笔记到sirius2alpha/Notes------------------------
 cd /home/yoho/projects/blogs/hugo/content/posts/Notes
 git add .
 git commit -m "update: $FILES"
 git pull
 git push
-echo end---同步笔记到sirius2alpha/Notes------------------------
+echo ------------------------end---同步笔记到sirius2alpha/Notes------------------------
 
 # sync dev-hugo branch 
-echo start---sync dev-hugo branch------------------------------
+echo ------------------------start---sync dev-hugo branch------------------------------
 cd /home/yoho/projects/blogs/hugo
 git add .
 git commit -m "update: $FILES"
 git pull
 git push
-echo end---sync dev-hugo branch------------------------------
+echo ------------------------end---sync dev-hugo branch------------------------------
 
 
 # build
@@ -25,12 +25,12 @@ cd ~/projects/blogs/hugo
 hugo
 
 # master branch only push
-echo start---master branch-------------------------------------
+echo ------------------------start---master branch-------------------------------------
 cd ~/projects/blogs/hugo/public
 git add .
 git commit -m "feat: $FILES"
 git push origin master
-echo end---master branch-------------------------------------
+echo ------------------------end---master branch-------------------------------------
 
 
 # 部署到 Nginx 服务器
